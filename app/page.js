b1eb2b42cc864c9a186143e4b8704ca173ec91f0
TanmayDabhade <tanmaydabhade129@gmail.com>
2024-08-03 16:31:08 +0530

    Production Now

--- app/page.js
+++ app/page.js
@@ -1,16 +1,30 @@
 'use client'
-import React, {useState, useEffect} from 'react';
-import { collection, addDoc, getDocs, getDoc, updateDoc, querySnapshot, query, onSnapshot, deleteDoc, doc, where } from "firebase/firestore"; 
+import React, { useState, useEffect } from 'react';
+import { collection, addDoc, getDocs, updateDoc, query, onSnapshot, deleteDoc, doc, where } from "firebase/firestore"; 
 import { db } from './firebase';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 
 export default function Home() {
 
-  const [items, setItems] = useState([])
-  
-  const [newItem, setNewItem] = useState({name: '', quantity: '', type: ''})
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState({ name: '', quantity: '', type: '' });
+  const [searchItem, setSearchItem] = useState('');
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-
-  const [searchItem, setSearchItem] = useState('')
+  const columns = [
+    { id: 'name', label: 'Item Name', minWidth: 100 },
+    { id: 'quantity', label: 'Quantity', minWidth: 100 },
+    { id: 'type', label: 'Type', minWidth: 100 },
+    { id: 'dateAdded', label: 'Date Added', minWidth: 150 },
+  ];
 
   // Add items to database
   const addItem = async (e) => {
@@ -22,12 +36,15 @@ export default function Home() {
 
         const querySnapshot = await getDocs(q);
 
+        const currentDate = new Date();
+
         if (!querySnapshot.empty) {
             // Item already exists, update the quantity
             querySnapshot.forEach(async (itemDoc) => {
                 const updatedQuantity = parseInt(itemDoc.data().quantity) + parseInt(newItem.quantity);
                 await updateDoc(doc(db, 'items', itemDoc.id), {
-                    quantity: updatedQuantity.toString()
+                    quantity: updatedQuantity.toString(),
+                    dateAdded: currentDate.toISOString(),
                 });
             });
         } else {
@@ -35,15 +52,16 @@ export default function Home() {
             await addDoc(itemsCollection, {
                 name: newItem.name.trim(),
                 quantity: newItem.quantity,
-                type: newItem.type
+                type: newItem.type,
+                dateAdded: currentDate.toISOString(),
             });
         }
 
         setNewItem({ name: '', quantity: '', type: '' });
     }
-};
+  };
 
-  // Add DeleteQuantity with icon
+  // Delete quantity or item
   const deleteQuantity = async (e) => {
     e.preventDefault();
 
@@ -62,7 +80,7 @@ export default function Home() {
                 if (updatedQuantity > 0) {
                     // Update the quantity
                     await updateDoc(doc(db, 'items', itemDoc.id), {
-                        quantity: updatedQuantity.toString()
+                        quantity: updatedQuantity.toString(),
                     });
                 } else {
                     // Delete the item if the resulting quantity is 0 or less
@@ -71,33 +89,24 @@ export default function Home() {
             });
         }
 
-        setNewItem({ name: '', quantity: '', type: ''});
+        setNewItem({ name: '', quantity: '', type: '' });
     }
-};
-
-  
+  };
 
   // Read items from database
-  useEffect(()=>{
-    const q = query(collection(db, 'items'))
-    const unsubscribe = onSnapshot(q, (querySnapshot)=>{
+  useEffect(() => {
+    const q = query(collection(db, 'items'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let itemsArr = [];
 
-      querySnapshot.forEach((doc)=>{
-        itemsArr.push({ ...doc.data(), id: doc.id});
+      querySnapshot.forEach((doc) => {
+        itemsArr.push({ ...doc.data(), id: doc.id });
       });
       setItems(itemsArr);
 
-      // read total from itemsArray
-      // const calculateTotal = () => {
-      //   const totalquantity = itemsArr.reduce((sum, item)=> sum + parseFloat(item.quantity), 0);
-      //   setTotal(totalquantity);
-      // };
-
-      calculateTotal();
       return () => unsubscribe();
     });
-  }, [])
+  }, []);
 
   // Handle search functionality
   const handleSearch = async (e) => {
@@ -112,75 +121,101 @@ export default function Home() {
     setItems(searchResults);
   };
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   return (
     <main className="w-full flex min-h-screen flex-col items-center justify-between sm:p-24 p-24">
-      <div className="z-10 max-w-7xl items-center justify-between font-mono  text-sm">
+      <div className="z-10 max-w-7xl items-center justify-between font-mono text-sm">
         <h1 className="text-4xl text-center p-4 ">Pantry Tracker</h1>
 
         <div className="bg-slate-800 p-4 rounded-lg w-full">
-          <form className=" grid grid-cols-12 items-center text-black">
+          <form className="grid grid-cols-12 items-center text-black">
             <input 
-            value={newItem.name}
-            onChange={(e) => setNewItem({...newItem, name: e.target.value})}
-            className='col-span-3 p-3 border' type="text" placeholder="Enter Item"/>
+              value={newItem.name}
+              onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
+              className='col-span-3 p-3 border rounded-lg' type="text" placeholder="Enter Item"/>
             <input 
-            value={newItem.quantity}
-            onChange={(e) => setNewItem({...newItem, quantity: e.target.value})}
-            className='col-span-2 p-3 border mx-3' type="number" placeholder="Enter Quantity"/>
+              value={newItem.quantity}
+              onChange={(e) => setNewItem({ ...newItem, quantity: e.target.value })}
+              className='col-span-2 p-3 border mx-3 rounded-lg' type="number" placeholder="Enter Quantity"/>
             <input 
-            value={newItem.type}
-            onChange={(e) => setNewItem({...newItem, type: e.target.value})}
-            className='col-span-3 p-3 border' type="text" placeholder="Enter Type"/>
-            
+              value={newItem.type}
+              onChange={(e) => setNewItem({ ...newItem, type: e.target.value })}
+              className='col-span-3 p-3 border rounded-lg' type="text" placeholder="Enter Type"/>
             <button 
-            onClick={addItem}
-            className="text-white col-span-2 mx-3 my-3 border-slate-900 border-2 hover:bg-slate-700 p-3 text-xl" type="submit">Add</button>
+              onClick={addItem}
+              className="text-white col-span-2 mx-3 my-3 border-slate-900 border-2 hover:bg-slate-700 p-2 text-xl rounded-lg" type="submit">Add</button>
             <button 
-            onClick={deleteQuantity}
-            className="text-white col-span-2 border-slate-900 border-2 hover:bg-slate-700 p-3 text-xl" type="submit">Delete</button>
-  
+              onClick={deleteQuantity}
+              className="text-white col-span-2 border-slate-900 border-2 hover:bg-slate-700 p-2 text-xl rounded-lg" type="submit">Delete</button>
           </form>
 
-          <form onSubmit={handleSearch} className="mt-4 grid grid-cols-12 items-center text-black">
+          <form onSubmit={handleSearch} className="mt-4 p-4 grid grid-cols-12 items-center text-black">
             <input 
               value={searchItem}
               onChange={(e) => setSearchItem(e.target.value)}
-              className='col-span-9 p-3 border' type="text" placeholder="Search"/>
+              className='col-span-9 mx-3 p-3 border rounded-lg' type="text" placeholder="Search"/>
             <button 
-              className="text-white col-span-3 border-slate-900 border-2 hover:bg-slate-700 p-3 text-xl" type="submit">Search</button>
+              className="text-white col-span-3 border-slate-900 border-2 hover:bg-slate-700 p-2 text-xl rounded-lg" type="submit">Search</button>
           </form>
-          <ul>
 
-          <div className='grid grid-cols-3 p-4 w-full text-center justify-between bg-slate-900 text-white'>
-            <span className='capitalize col-span-1'>Item Name</span>
-              <span className='text-center col-span-1'>Quantity</span>
-              <span className='capitalize col-span-1'>Type</span>
-            </div>
-            {items.map((item, id) => (
-              <>
-              
-              <li key = {id} className='text-white my-4 w-full flex justify-between bg-slate-950'>
-                
-                <div className='grid grid-cols-3 p-4 w-full justify-between text-center'>
-                  <span className='capitalize col-span-1'>{item.name}</span>
-                  <span className='text-center col-span-1'>{item.quantity}</span>
-                  <span className='capitalize col-span-1'>{item.type}</span>
-                </div>
-                {/* <button onClick={()=>deleteItem(item.id)} className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16'>X</button> */}
-              </li>
-              </>
-              
-            ))}
-          </ul>
-          
+          <Paper  sx={{ width: '100%', overflow: 'hidden', textAlign: 'center' }}>
+            <TableContainer sx={{ maxHeight: 440 }}>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    {columns.map((column) => (
+                      <TableCell
+                        key={column.id}
+                        align="center"
+                        style={{ minWidth: column.minWidth }}
+                      >
+                        {column.label}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {items.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                    return (
+                      <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                        {columns.map((column) => {
+                          const value = row[column.id];
+                          return (
+                            <TableCell key={column.id} align="center">
+                              {column.id === 'dateAdded' ? new Date(value).toLocaleString() : value}
+                            </TableCell>
+                          );
+                        })}
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={items.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </Paper>
         </div>
-        
       </div>
 
       <footer>
-        <p className='text-xl'>Made by <a className='text-blue-600 ' href = 'https://www.tanmaydabhade.com'>Tanmay Dabhade</a></p>
+        <p className='text-xl'>Made by <a className='text-blue-600 ' href='https://www.tanmaydabhade.com'>Tanmay Dabhade</a></p>
       </footer>
     </main>
-  )
+  );
 }
